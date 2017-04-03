bc9fccd38310e9542d5aab793d9c09fb7ff393ca
Lyuki <s1153111@ouhk.edu.hk>
2017-04-03 10:46:07 +0800

    Add files via upload

---
+++ src/main/java/com/mycompany/webapp/model/AllUser.java
@@ -0,0 +1,46 @@
+package com.mycompany.webapp.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class AllUser {
+    private String username;
+    private String password;
+    private List<String> roles = new ArrayList<>();
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    public List<String> getRoles() {
+
+        return roles;
+
+    }
+
+    public void addRole(String role) {
+
+        this.roles.add(role);
+
+    }
+
+    public boolean hasRole(String role) {
+
+        return this.roles.contains(role);
+
+    }
+
+}
