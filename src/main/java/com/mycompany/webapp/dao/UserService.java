f0b42341578c0f89faaf66c96b7d4a8a8cf1b252
Lyuki <s1153111@ouhk.edu.hk>
2017-04-05 20:17:40 +0800

    Add files via upload

---
+++ src/main/java/com/mycompany/webapp/dao/UserService.java
@@ -0,0 +1,38 @@
+package com.mycompany.webapp.dao;
+
+import com.mycompany.webapp.model.AllUser;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+
+public class UserService implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepo;
+    
+    @Override
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+
+        AllUser user = userRepo.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User '" + username + "' not found.");
+        }
+
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        for (String role : user.getRoles()) {
+            authorities.add(new SimpleGrantedAuthority(role));
+        }
+
+        return new User(user.getUsername(), user.getPassword(), authorities);
+    }
+}
