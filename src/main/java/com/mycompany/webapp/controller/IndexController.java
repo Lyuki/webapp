92a1af42437ba66ba04399bdec0c0134271b9843
Rainki <Rainki@192.168.31.239>
2017-04-07 01:39:40 +0900

    new Commit

--- src/main/java/com/mycompany/webapp/controller/IndexController.java
+++ src/main/java/com/mycompany/webapp/controller/IndexController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.View;
 import org.springframework.web.servlet.view.RedirectView;
+import com.mycompany.webapp.model.Topics;
 
 @Controller
 public class IndexController {
@@ -21,6 +22,27 @@ public class IndexController {
     @RequestMapping("create")
     public String addUser() {
         return "addUser";
+    }
+    
+    @RequestMapping("Lecture")
+    public String Lecturetopics() {
+        Topics topic = new Topics();
+        topic.setCategory("Lecture");
+        return "topic";
+    }
+    
+    @RequestMapping("Lab")
+    public String Labtopics() {
+        Topics topic = new Topics();
+        topic.setCategory("Lab");
+        return "topic";
+    }
+    
+    @RequestMapping("Other")
+    public String Othertopics() {
+        Topics topic = new Topics();
+        topic.setCategory("Other");
+        return "topic";
     }
 }
 
