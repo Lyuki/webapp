1873669c6c19d5183949f7abb51c0def77ad76d7
Rainki <Rainki@192.168.31.239>
2017-04-11 01:24:08 +0900

    new Commit

--- src/main/java/com/mycompany/webapp/dao/TopicRepository.java
+++ src/main/java/com/mycompany/webapp/dao/TopicRepository.java
@@ -7,7 +7,7 @@ import java.util.List;
 public interface TopicRepository {
     public void create(Topics topic);
     public void editByID(Topics topic);
-    public List<Topics> findAll();
+    public List<Topics> findAll(String cate);
     public Topics findByID(long id);
     public void deleteByID(long id);
 }
