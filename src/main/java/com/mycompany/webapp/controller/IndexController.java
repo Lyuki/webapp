f03d86f38207de32a473724b4dada65fcca79494
Lyuki <s1153111@ouhk.edu.hk>
2017-04-10 18:30:54 +0800

    Update IndexController.java

--- src/main/java/com/mycompany/webapp/controller/IndexController.java
+++ src/main/java/com/mycompany/webapp/controller/IndexController.java
@@ -1,9 +1,14 @@
 package com.mycompany.webapp.controller;
 
+import com.mycompany.webapp.dao.PollRepository;
+import com.mycompany.webapp.model.Poll;
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.view.RedirectView;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.View;
 
 @Controller
