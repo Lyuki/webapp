d058051927cded6ad04050d3d034765b9f7e5360
Lyuki <s1153111@ouhk.edu.hk>
2017-04-10 18:23:41 +0800

    Update Poll.java

--- src/main/java/com/mycompany/webapp/model/Poll.java
+++ src/main/java/com/mycompany/webapp/model/Poll.java
@@ -2,12 +2,13 @@ package com.mycompany.webapp.model;
 
 public class Poll {
     private long id;
+    private String customerName;
     private String question;
     private String ans1;
     private String ans2;
     private String ans3;
     private String ans4;
-    
+
     public long getId() {
         return id;
     }
@@ -16,45 +17,51 @@ public class Poll {
         this.id = id;
     }
 
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+    
     public String getQuestion() {
-      return question;
+        return question;
     }
 
     public void setQuestion(String question) {
-      this.question = question;
+        this.question = question;
     }
 
     public String getAns1() {
-      return ans1;
+        return ans1;
     }
 
     public void setAns1(String ans1) {
-      this.ans1 = ans1;
+        this.ans1 = ans1;
     }
 
     public String getAns2() {
-      return ans2;
+        return ans2;
     }
 
     public void setAns2(String ans2) {
-      this.ans2 = ans2;
+        this.ans2 = ans2;
     }
 
     public String getAns3() {
-      return ans3;
+        return ans3;
     }
 
     public void setAns3(String ans3) {
-      this.ans3 = ans3;
+        this.ans3 = ans3;
     }
 
     public String getAns4() {
-      return ans4;
+        return ans4;
     }
 
     public void setAns4(String ans4) {
-      this.ans4 = ans4;
+        this.ans4 = ans4;
     }
-    
-    
 }
